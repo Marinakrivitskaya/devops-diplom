a08bd969e78d9eb7dea29652eb5fb35069d57f66
Marina Krivitskaya <67638098+Marinakrivitskaya@users.noreply.github.com>
2021-11-03 13:02:32 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,13 +28,13 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "diploma-vpc"
-  cidr = "172.31.0.0/16"
+  cidr = "10.0.0.0/16"
 
   azs = ["us-east-2a"]
-  public_subnets = ["172.31.0.0/24"]
+  public_subnets = ["10.0.101.0/24"]
 
-  enable_nat_gateaway = true
-  enable_vpn_gareaway = false
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
 
   tags = {
     Terraform = "true"
